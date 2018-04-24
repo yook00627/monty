ed7a279ddfeea7d6464e96fbf9fe2c3a31c0da21
Andrew S <andrew.suh.94@gmail.com>
2018-04-24 15:25:18 -0700

    Added argc check for proper usage

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+
+int main(int argc, char *argv)
+{
+    if (argc != 2)
+    {
+        printf("USAGE: monty file")
+        exit(EXIT_FAILURE)
+    }
+    fd = open(argv[1])
+}

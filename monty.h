dc81bc6848c982014d9ae95c189256a8df03703a
Kevin Yook <yook00627@gmail.com>
2018-04-24 22:20:10 +0000

    added more header

--- monty.h
+++ monty.h
@@ -3,7 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)

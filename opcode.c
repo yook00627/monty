34be67de4085f5a835f0cc04c969cf5e902a032f
Kevin Yook <yook00627@gmail.com>
2018-04-25 04:46:38 +0000

    created opcode file and created push

---
+++ opcode.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+void opcode(char *command, unsigned int line_num, char *str, stack_t **stack)
+{
+	int i = 0;
+	instruction_t ops[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+
+	while (ops[i].opcode != NULL)
+	{
+		if (strcmp(ops[i].opcode, command) == 0)
+		{
+			ops[i].f(stack, line_num);
+			
+		}
+		i++;
+	}
+
+}
--- push.c
+++ push.c
@@ -4,3 +4,26 @@
  *
  *
  */
+void push(stack_t **stack, unsigned int line_num)
+{
+	dlistint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+	(*head)->prev = new;
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+	return (new);
+}

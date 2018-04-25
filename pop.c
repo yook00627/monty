9f1482c6a98b85ac85a54dc91f21a4608eec2c28
Kevin Yook <yook00627@gmail.com>
2018-04-25 23:05:28 +0000

    print error and free properly

--- pop.c
+++ pop.c
@@ -4,11 +4,10 @@ void pop(stack_t **stack, unsigned int line_num)
 {
 	stack_t *next = NULL;
 
-	if (*stack == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
 		printf("L%d: can't pop an empty stack\n", line_num);
-		free_stack(stack);
-		exit(EXIT_FAILURE);
+		return;
 	}
 
 	next = (*stack)->next;

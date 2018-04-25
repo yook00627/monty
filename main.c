2821cbbdf36a2788930d1ac051cc2544bbf2106f
Kevin Yook <yook00627@gmail.com>
2018-04-25 01:56:13 +0000

    change the file

--- main.c
+++ main.c
@@ -2,18 +2,27 @@
 
 int main(int argc, char **argv)
 {
-    int fd;
+	int file_size = 0;
+	FILE *file_input;
+	char *str;
 
-    if (argc != 2)
-    {
-        printf("USAGE: monty file");
-        exit(EXIT_FAILURE);
-    }
-    fd = fopen(argv[1], "r");
-    if (fd == -1)
-    {
-        printf("Error: Can't open file %s", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file_input = fopen(argv[1], "r");
+	if (file_input == NULL)
+	{
+		printf("Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	fseek(file_input, 0, SEEK_END);
+	file_size = ftell(file_input);
+	rewind(file_input);
+	str = malloc(sizeof(char) * file_size);
+	fread(str, file_size, 1, file_input);
+	printf("%s\n", str);
+	fclose(file_input);
+	return (0);
 }

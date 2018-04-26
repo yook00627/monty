24873bb663261ea81fc34a0589063eef5b41c158
Kevin Yook <yook00627@gmail.com>
2018-04-26 02:09:10 +0000

    create comments file

---
+++ new code files/comments.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * comments - ignore comments
+ * @stack: input stack address
+ * @line_num: current line number
+ */
+void comments(stack_t **stack, unsigned int line_num)
+{
+	(void) stack;
+	(void) line_num;
+}
